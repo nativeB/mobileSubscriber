2f8706b9c780ebe9f3350c94fcb1b282b5d1f92b
quincy <qhutchison8@gmail.com>
2022-03-25 23:53:44 +0000

    add route /subscriber/:subscriberId

---
+++ src/controller/subscriber/get.id.subscribers.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from 'express';
+import { getOneSubscriber } from '../../services';
+import { getObjectId } from '../../utils';
+
+export const getSubscriberById =  async (req: Request, res: Response) => {
+    const  subscriberId = req.params.subscriberId;
+
+    const subscriber = await getOneSubscriber({id : getObjectId(subscriberId) })
+
+    return res.json({
+      subscriber
+    })
+  };
