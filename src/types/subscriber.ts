91425f87aa373c1ea9a494a1fdceebbf6fdce684
quincy <qhutchison8@gmail.com>
2022-03-25 23:43:10 +0000

    add constants file

---
+++ src/types/subscriber.ts
@@ -0,0 +1,1 @@
+export type ServiceTypes = "MOBILE_PREPAID"| "MOBILE_POSTPAID"
---
+++ src/utils/constants.ts
@@ -0,0 +1,10 @@
+import { ServiceTypes } from "../types/subscriber";
+
+
+const SERVICE_TYPES: ServiceTypes[]  = [ "MOBILE_PREPAID", "MOBILE_POSTPAID"]
+
+const constants = Object.freeze({
+    SERVICE_TYPES
+})
+
+export default constants;
