b5ec690fb830e3f0b46c304f9c9ba712ec6f6aa3
quincy <qhutchison8@gmail.com>
2022-03-26 10:55:44 +0000

    updates to entry file

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,33 @@ import * as express from 'express';
 import  * as mongoose from 'mongoose'
 import { json } from 'body-parser';
 import router from './routes'
+import constants from './utils/constants';
+import * as morgan from "morgan"
 
 const app = express()
 app.use(json())
+
+app.use(morgan('tiny'));
 app.use('/', router)
 
+app.use(( _req: express.Request, res:express.Response) => {
+
+  res.status(500).send({
+    success:false,
+    message: constants.STATUS_CODES[500]
+  })
+})
 mongoose.connect(process.env.MONGO_URI as string, {
 }, () => {
   console.log('connected to database')
 })
 
-app.listen(3000, () => {
+const server = app.listen(3000, () => {
   console.log('server is listening on port 3000')
+})
+
+process.on('SIGTERM', () => {
+  server.close(() => {
+    console.log('HTTP server closed')
+  })
 })
